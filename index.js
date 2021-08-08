ad89fadecc66831c8e4c3207e3e0fb4c517b47c5
Mudit Gupta <guptamudit@ymail.com>
2021-08-08 22:25:39 +0530

    Basic checking

---
+++ index.js
@@ -0,0 +1,67 @@
+const config = require("./config.json");
+const request = require('request');
+const baseUrl = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?";
+
+let districtToProcess = 0;
+
+function main() {
+    console.log("Starting the script. Please do not close it if you want to get notified.");
+    // 100 Requests allowed per 5 min -> A request can be made every 3 seconds (Ignoring burst)
+    // To play it safe, we'll be making a request every 5 seconds
+    setInterval(processDistrict, 5000);
+}
+
+function processDistrict() {
+    const requestUrl = baseUrl + "district_id=" + config.districts[districtToProcess] + "&date=" + getDate();
+    console.log("Checking district", config.districts[districtToProcess]);
+    request(requestUrl, { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+
+        body.centers.forEach(center => {
+            let sessionFound = false;
+            center.sessions.forEach(session => {
+                const availableSlots = sessionAvailability(session);
+                if(availableSlots > 0) {
+                    if (!sessionFound) {
+                        console.log(center.name);
+                        console.log(center.address);
+                        console.log(center.district_name + ", " + center.state_name + " - " + center.pincode);
+                        sessionFound = true;
+                    }
+                    console.log(availableSlots + " slots available on " + session.date);
+                    console.log(session.slots);
+                }
+            });
+        });
+    });
+
+    districtToProcess++;
+    if (districtToProcess >= config.districts.length) {
+        districtToProcess = 0;
+    }
+}
+
+function getDate() {
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    const yyyy = today.getFullYear();
+
+    return dd + '-' + mm + '-' + yyyy;
+}
+
+function sessionAvailability(session) {
+    if (!session.vaccine.toLowerCase().includes(config.vaccine.toLowerCase())) {
+        return 0;
+    }
+    if ((session.min_age_limit && session.min_age_limit > config.age) || (session.max_age_limit && session.max_age_limit < config.age)) {
+        return 0;
+    }
+    if (config.dose == 1) {
+        return session.available_capacity_dose1;
+    } else {
+        return session.available_capacity_dose2;
+    }
+}
+
+main();
